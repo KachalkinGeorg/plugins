7699f6a97251c94af8176cae0c70d2cf9b58282f
KachalkinGeorg <119577414+KachalkinGeorg@users.noreply.github.com>
2023-02-20 06:57:57 +0300

    Кто сейчас в онлайн

---
+++ k_online/uninstall.php
@@ -0,0 +1,21 @@
+<?php
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+//
+// Configuration file for plugin
+//
+pluginsLoadConfig();
+$db_update = array(
+	array(
+		'table'  => 'k_online',
+		'action' => 'drop',
+	),
+);
+if ($_REQUEST['action'] == 'commit') {
+	if (fixdb_plugin_install($plugin, $db_update, 'deinstall')) {
+		plugin_mark_deinstalled($plugin);
+	}
+} else {
+	generate_install_page($plugin, 'Удаление плагина', 'deinstall');
+}
+?>
