f99ba3b668b15e1c43fbf9b111e849417d02f65a
KachalkinGeorg <119577414+KachalkinGeorg@users.noreply.github.com>
2023-02-20 07:03:01 +0300

    Цитатник

---
+++ quotes/uninstall.php
@@ -0,0 +1,25 @@
+<?php
+// Protect against hack attempts
+if (!defined('NGCMS')) die ('HAL');
+//
+// Configuration file for plugin
+//
+pluginsLoadConfig();
+$db_update = array(
+	array(
+		'table'  => 'quotes',
+		'action' => 'drop',
+	),
+	array(
+		'table'  => 'quotes_flood',
+		'action' => 'drop',
+	),
+);
+if ($_REQUEST['action'] == 'commit') {
+	if (fixdb_plugin_install($plugin, $db_update, 'deinstall')) {
+		plugin_mark_deinstalled($plugin);
+	}
+} else {
+	generate_install_page($plugin, 'Удаление плагина', 'deinstall');
+}
+?>
